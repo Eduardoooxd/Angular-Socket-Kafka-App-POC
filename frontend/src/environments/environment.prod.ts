8754fdfe4ddb94852a1ffe517fd99182716045c7
Eduardo Couto (CTW) <Eduardo.MR.Couto@ctw.bmwgroup.com>
2023-02-21 19:49:38 +0000

    chore: Creating docker file for each microservice and all necessary configs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  webSocketServer: null,
-  producerServer: null,
+  // webSocketServer: 'ws://websocket-consumer:9090/websocket/',
+  // producerServer: 'http://producer:8080/producer',
+  webSocketServer: 'ws://localhost:9090/websocket/',
+  producerServer: 'http://localhost:8080/producer',
 };
